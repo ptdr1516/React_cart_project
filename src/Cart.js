6517c191b62b8e68fc1d4b84c7c3cd65961ce99e
ptdr1516 <71786648+ptdr1516@users.noreply.github.com>
2023-01-06 23:00:04 +0530

    hanldling inc. and dec. quantity

--- src/Cart.js
+++ src/Cart.js
@@ -33,6 +33,39 @@ class Cart extends React.Component {
         // this.testing();
     }
 
+    handleIncreaseQuantity = (product) => {
+        console.log('Hey please inc the quantity of',product);
+        const { products } = this.state;
+        const index = products.indexOf(product);
+
+        products[index].qty += 1;
+        
+        this.setState({ 
+            products 
+        });
+
+    }
+
+    handleDecreaseQuantity = (product) => {
+        
+
+        console.log('Hey please dec the quantity of',product);
+        const { products } = this.state;
+        const index = products.indexOf(product);
+        
+        if (products[index].qty === 0) {
+            return;
+        }
+
+        products[index].qty -= 1;
+
+        
+        this.setState({ 
+            products 
+        });
+
+    }
+
    render () {
     const { products } = this.state;
     return (
@@ -42,7 +75,9 @@ class Cart extends React.Component {
                 return(
                     <CartItem 
                     product = {product} 
-                    key={product.id} 
+                    key={product.id}
+                    onIncreaseQuantity={this.handleIncreaseQuantity}
+                    onDecreaseQuantity={this.handleDecreaseQuantity}
                     />
                 ); 
             })}
--- src/CartItem.js
+++ src/CartItem.js
@@ -2,51 +2,8 @@ import React from "react";
 import './index.css';
 
 class CartItem extends React.Component {
-    
-    // testing () {
-    //     const promise = new Promise((resolve, reject) => {
-    //         setTimeout(() => {
-    //             resolve('done');
-    //         }, 5000);
-    //     })
-
-    //     promise.then(() => {
-    //         // setState acts like a synchronous call
-    //         this.setState({ qty: 100 });
-    //         console.log('state', this.state);
-    //     });
-    // }
-
-    increaseQuantity = () => {
-        // this.state.qty += 1;
-        // setState form 1
-        // this.setState({
-        //     qty: this.state.qty + 1
-        // })
-
-        // setState form 2 - if prevState is required
-        this.setState((prevState) => {
-            return {
-                qty: this.state.qty + 1
-            }
-        }, () => {
-            console.log('this.state', this.state);
-        });
-    }
-
-    decreaseQuantity = () => {
-        const { qty } = this.state;
-        if (qty === 0) {
-            return;
-        }
-        this.setState((prevState) => {
-            return {
-                qty: this.state.qty - 1
-            }
-        });
-    }
-
     render() {
+        console.log('this.props', this.props)
         const { price, title, qty } = this.props.product;
         return (
             <div className="cart-item">
@@ -60,9 +17,9 @@ class CartItem extends React.Component {
                     <div className="cart-item-actions">
                         {/* Buttons */}
                         <img alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992651.png"
-                        onClick={this.increaseQuantity.bind(this)} />
+                        onClick={() => this.props.onIncreaseQuantity(this.props.product)} />
                         <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992683.png" 
-                        onClick={this.decreaseQuantity.bind(this)}/>
+                        onClick={() => this.props.onDecreaseQuantity(this.props.product)}/>
                         <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/3405/3405244.png" />
                     </div>
                 </div>
