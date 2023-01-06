07f3f64986c6cd715110feeb1754989bac0426f3
ptdr1516 <71786648+ptdr1516@users.noreply.github.com>
2023-01-06 22:38:15 +0530

    add props to cart item

--- src/CartItem.js
+++ src/CartItem.js
@@ -2,18 +2,7 @@ import React from "react";
 import './index.css';
 
 class CartItem extends React.Component {
-    constructor () {
-        super();
-        this.state = {
-            price: 999,
-            title: 'Mobile Phone',
-            qty: 1,
-            img: ''
-        }
-        // this.increaseQuantity = this.increaseQuantity.bind(this);
-        // this.testing();
-    }
-
+    
     // testing () {
     //     const promise = new Promise((resolve, reject) => {
     //         setTimeout(() => {
@@ -58,7 +47,7 @@ class CartItem extends React.Component {
     }
 
     render() {
-        const { price, title, qty } = this.state;
+        const { price, title, qty } = this.props.product;
         return (
             <div className="cart-item">
                 <div className="left-block">
