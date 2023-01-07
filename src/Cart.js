f27e089e54eef650bff7661b7fbcae33a39a6792
ptdr1516 <71786648+ptdr1516@users.noreply.github.com>
2023-01-07 14:23:10 +0530

    Moving cart fun to app and adding cart count

--- src/Cart.js
+++ src/Cart.js
@@ -1,85 +1,9 @@
 import React from "react";
 import CartItem from "./CartItem";
 
-class Cart extends React.Component {
-    constructor () {
-        super();
-        this.state = {
-            products: [
-                {
-                    price: 99,
-                    title: 'Watch',
-                    qty: 1,
-                    img: '',
-                    id: 1
-                },
-                {
-                    price: 999,
-                    title: 'Mobile Phone',
-                    qty: 1,
-                    img: '',
-                    id: 2
-                },
-                {
-                    price: 999,
-                    title: 'Laptop',
-                    qty: 1,
-                    img: '',
-                    id: 3
-                },
-            ]
-        }
-        // this.increaseQuantity = this.increaseQuantity.bind(this);
-        // this.testing();
-    }
-
-    handleIncreaseQuantity = (product) => {
-        console.log('Hey please inc the quantity of',product);
-        const { products } = this.state;
-        const index = products.indexOf(product);
-
-        products[index].qty += 1;
-        
-        this.setState({ 
-            products 
-        });
-
-    }
-
-    handleDecreaseQuantity = (product) => {
-        
-
-        console.log('Hey please dec the quantity of',product);
-        const { products } = this.state;
-        const index = products.indexOf(product);
-        
-        if (products[index].qty === 0) {
-            return;
-        }
-
-        products[index].qty -= 1;
-
-        
-        this.setState({ 
-            products 
-        });
-
-    }
-
-    handleDeleteProduct = (id) => {
-        const { products } = this.state;
-        // Filter products by id.
-        // Checks if the item has the same id as the item which has to be deleted.
-        const items = products.filter((item) => item.id !== id);
-
-        this.setState({ 
-            products: items
-        });
-        
-    }
-
-   render () {
-    const { products } = this.state;
+const Cart = (props) => {
+    
+    const { products } = props;
     return (
         <div className="cart">
             
@@ -88,15 +12,14 @@ class Cart extends React.Component {
                     <CartItem 
                     product = {product} 
                     key={product.id}
-                    onIncreaseQuantity={this.handleIncreaseQuantity}
-                    onDecreaseQuantity={this.handleDecreaseQuantity}
-                    onDeleteProduct={this.handleDeleteProduct}
+                    onIncreaseQuantity={props.onIncreaseQuantity}
+                    onDecreaseQuantity={props.onDecreaseQuantity}
+                    onDeleteProduct={props.onDeleteProduct}
                     />
                 ); 
             })}
         </div>
     );
-   }
 }
 
 export default Cart;
--- src/Navbar.js
+++ src/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = (props) => {
         <div style={styles.nav}>
             <div style={styles.cart}>
                 <img style={styles.cartIcon} src="https://cdn-icons-png.flaticon.com/512/3144/3144456.png" alt="cart-icon" />
-                <span style={styles.cartCount}>3</span>
+                <span style={styles.cartCount}>{ props.count }</span>
             </div>
         </div>
     );
