0b289354742307aa761a11e0143277ce27203ada
ptdr1516 <71786648+ptdr1516@users.noreply.github.com>
2023-01-07 13:51:07 +0530

    Add Navbar component more

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,21 +1,20 @@
 import React from "react";
 import './index.css';
 
-class CartItem extends React.Component {
-    render() {
-        console.log('this.props', this.props)
-        const { price, title, qty } = this.props.product;
+const CartItem = (props) => {
+
+        const { price, title, qty } = props.product;
         const { 
             product, 
             onIncreaseQuantity, 
             onDecreaseQuantity, 
             onDeleteProduct 
-        } = this.props;
+        } = props;
 
         return (
             <div className="cart-item">
                 <div className="left-block">
-                    <img style={styles.image}/>
+                    <img style={styles.image} />
                 </div>
                 <div className="right-block">
                     <div style={ {fontSize: 25} }>{title}</div>
@@ -33,7 +32,6 @@ class CartItem extends React.Component {
                 </div>
             </div>
         );
-    }
 }
 
 // creating object to hold style properties
