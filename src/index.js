b433f9ccbfc6250e6060ef7689c21306dcc6f014
ptdr1516 <71786648+ptdr1516@users.noreply.github.com>
2023-01-09 18:37:12 +0530

    Added firebase

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import 'firebase/firestore';
+import { initializeApp } from "firebase/app";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDVaqj7HYXuXNt_ac4i6rU-e9biwsoqbDQ",
+  authDomain: "cart-62a4e.firebaseapp.com",
+  projectId: "cart-62a4e",
+  storageBucket: "cart-62a4e.appspot.com",
+  messagingSenderId: "991551718630",
+  appId: "1:991551718630:web:ef6704bcfff760fe5d6727"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
